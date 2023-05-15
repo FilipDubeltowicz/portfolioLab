da306bb45e8df8cfe41e974d4beca07e43a5d782
FilipDubeltowicz <f.dubeltowicz@gmail.com>
2023-05-15 10:14:11 +0200

    Donation entity added

---
+++ src/main/java/pl/coderslab/charity/Donation.java
@@ -0,0 +1,32 @@
+package pl.coderslab.charity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "donation")
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    @OneToMany
+    private List<Category> categories;
+    @ManyToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+}
