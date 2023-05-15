3c77a69514c05187d4bd95d84ca89d9172069fa8
FilipDubeltowicz <f.dubeltowicz@gmail.com>
2023-05-15 09:34:38 +0200

    project upload

---
+++ src/main/java/pl/coderslab/charity/Category.java
@@ -0,0 +1,24 @@
+package pl.coderslab.charity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "category")
+public class Category {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
